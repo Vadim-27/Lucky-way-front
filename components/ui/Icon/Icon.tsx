905eb475f57bcab083d8e0ec60aa9fedd27c15b2
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-29 11:17:00 +0300

    added Icon component

---
+++ components/ui/Icon/Icon.tsx
@@ -0,0 +1,18 @@
+import { FC, SVGProps } from 'react';
+
+type IconVariant = 'low-emission-zone' | 'payment-of-fines' | 'toll-roads';
+
+interface IIcon extends SVGProps<SVGSVGElement> {
+    variant: IconVariant;
+    className?: string;
+}
+
+const Icon: FC<IIcon> = ({ variant, className }) => {
+    return (
+        <svg className={className}>
+            <use href={`/icons/icons.svg#icon-${variant}`}></use>
+        </svg>
+    );
+};
+
+export default Icon;
