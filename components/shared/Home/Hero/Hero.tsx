a9460108b147458a19f4866dec1792cfdaf6b4a4
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-30 20:06:18 +0300

    upd

--- components/shared/Home/Hero/Hero.tsx
+++ components/shared/Home/Hero/Hero.tsx
@@ -1,14 +1,18 @@
+import { useTranslations } from 'next-intl';
+
 import Button from '@/components/ui/Button';
 import Container from '@/components/ui/Container';
 
 import scss from './Hero.module.scss';
 
 const Hero = () => {
+    const t = useTranslations('Hero');
+
     return (
         <section className={scss.hero}>
             <Container>
                 <div className={scss.wrapper}>
-                    <h1 className={scss.title}>Start Your Easy Way with Company Name</h1>
+                    <h1 className={scss.title}>{t('title')}</h1>
                     <p className={scss.description}>
                         Purchase vignettes for toll roads, explore green zones in cities, and pay
                         fines effortlessly. Travel smart, drive smoothly.
