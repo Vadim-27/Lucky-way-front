cb8e08bbffe2e70123c11eeb15d8ec7724c39921
Vadim-27 <vadim.bulanyi@gmail.com>
2024-11-13 21:21:27 +0100

    add options language

--- components/shared/Header/LangSwitcher/options.tsx
+++ components/shared/Header/LangSwitcher/options.tsx
@@ -25,7 +25,7 @@ const options: IOption[] = [
         label: (
             <div className={scss.option}>
                 <Icon variant="de" className={scss.icon} />
-                <span className={clsx(dmSans.className, scss.lang)}>deu</span>
+                <span className={clsx(dmSans.className, scss.lang)}>DEU</span>
             </div>
         ),
     },
@@ -34,7 +34,187 @@ const options: IOption[] = [
         label: (
             <div className={scss.option}>
                 <Icon variant="cz" className={scss.icon} />
-                <span className={clsx(dmSans.className, scss.lang)}>cze</span>
+                <span className={clsx(dmSans.className, scss.lang)}>CZE</span>
+            </div>
+        ),
+    },
+    {
+        value: 'uk',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>UKR</span>
+            </div>
+        ),
+    },
+    {
+        value: 'sk',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="de" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>SLO</span>
+            </div>
+        ),
+    },
+    {
+        value: 'pl',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>POL</span>
+            </div>
+        ),
+    },
+    {
+        value: 'lv',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>LAV</span>
+            </div>
+        ),
+    },
+    {
+        value: 'lt',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="de" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>LIT</span>
+            </div>
+        ),
+    },
+    {
+        value: 'et',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>EST</span>
+            </div>
+        ),
+    },
+    {
+        value: 'hu',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>HUN</span>
+            </div>
+        ),
+    },
+    {
+        value: 'ro',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>RUM</span>
+            </div>
+        ),
+    },
+    {
+        value: 'bg',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="de" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>BUL</span>
+            </div>
+        ),
+    },
+    {
+        value: 'tr',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>TUR</span>
+            </div>
+        ),
+    },
+    {
+        value: 'sl',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>SLV</span>
+            </div>
+        ),
+    },
+    {
+        value: 'da',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="de" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>DAN</span>
+            </div>
+        ),
+    },
+    {
+        value: 'nl',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>NLD</span>
+            </div>
+        ),
+    },
+    {
+        value: 'fr',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>FRA</span>
+            </div>
+        ),
+    },
+    {
+        value: 'it',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="de" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>ITA</span>
+            </div>
+        ),
+    },
+    {
+        value: 'ru',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>RUS</span>
+            </div>
+        ),
+    },
+    {
+        value: 'es',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="en" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>SPA</span>
+            </div>
+        ),
+    },
+    {
+        value: 'no',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="de" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>NOR</span>
+            </div>
+        ),
+    },
+    {
+        value: 'sv',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>SWE</span>
+            </div>
+        ),
+    },
+    {
+        value: 'fi',
+        label: (
+            <div className={scss.option}>
+                <Icon variant="cz" className={scss.icon} />
+                <span className={clsx(dmSans.className, scss.lang)}>FIN</span>
             </div>
         ),
     },
